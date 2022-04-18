fdca8c0ed359b98796e709fc641be12a4cb41764
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2022-04-18 02:40:24 -0500

    Update torrent-clients-installer.sh

--- torrent-clients-installer.sh
+++ torrent-clients-installer.sh
@@ -1,18 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
 clear
 
 echo "
-######   ######   ##  ##    ####              ####    ##  ##    ####    ######     ##     ##       ##       ######   #####   
-     ##   ##       ##  ##   ##  ##              ##     ### ##   ##  ##     ##      ####    ##       ##       ##       ##  ##  
-    ##    ##       ##  ##   ##                  ##     ######   ##         ##     ##  ##   ##       ##       ##       ##  ##  
-   ##     ####     ##  ##    ####               ##     ######    ####      ##     ######   ##       ##       ####     #####   
-  ##      ##       ##  ##       ##              ##     ## ###       ##     ##     ##  ##   ##       ##       ##       ####    
- ##       ##       ##  ##   ##  ##              ##     ##  ##   ##  ##     ##     ##  ##   ##       ##       ##       ## ##   
- ######   ######    ####     ####              ####    ##  ##    ####      ##     ##  ##   ######   ######   ######   ##  ##  
-                                                                                                                              
-"
  
+___  ____ _  _ ____    _ _  _ ____ ___ ____ _    _    ____ ____ 
+  /  |___ |  | [__     | |\ | [__   |  |__| |    |    |___ |__/ 
+ /__ |___ |__| ___]    | | \| ___]  |  |  | |___ |___ |___ |  \ 
+ 
+"
+
 #This script will install qbittorrent.
 
 ######################################################################
@@ -41,8 +38,8 @@ echo " "
 echo " "
 echo "Would you like to update your system (Recommended)? (y/n/e)"
 echo " "
-echo "y=yes | n=no | e=exit-program"
-read yesorno
+echo "y=yes | n=no | f=Change-container-volumes-&-location | e=exit-program"
+read -n1 yesorno
 
 if [ "$yesorno" = y ]; then
 	updatesys
@@ -63,7 +60,7 @@ fi
 
 echo "Would you like to check if Docker is working(Recommended)? (y/n/e)"
 
-read yesorno
+read -n1 yesorno
 
 if [ "$yesorno" = y ]; then
 	echo 'Checking Docker version...'
@@ -87,9 +84,9 @@ fi
 
 #Install qBittorrent
 
-echo "Would you like install qBittorrent? (y/n/e)"
+echo "Would you like install qBittorrent? (y/n/f/e)"
 
-read yesorno
+read -n1 yesorno
 
 if [ "$yesorno" = y ]; then
 	mkdir /home/$USER/raspi-docker/qbittorrent
@@ -111,6 +108,40 @@ if [ "$yesorno" = y ]; then
 	echo "Successfully Added"
 elif [ "$yesorno" = n ]; then
 	echo "Skipping..."
+elif [ "$yesorno" = f ]; then
+          echo " "
+          read -n1 -p "You have selected to change the location/volumes of the container. Would you like to coninue? (y/n) " fix
+  		if [ "$fix" = y ]; then
+        echo " "
+        read -p "Enter the location of the docker-compose.yml file: " qbitanswer
+  			read -p "Enter the new location for config: " qbitconfig
+        read -p "Enter the new location for downloads: " qbitdl
+  			sleep 1
+  			echo "qbittorrent:
+          container_name: qbittorrent
+          image: hotio/qbittorrent
+          ports:
+            - 8282:8080
+          environment:
+            - PUID=1001
+            - PGID=100
+            - UMASK=002
+            - TZ=US/Central
+          volumes:
+            - $qbitocnfig:/config
+            - $qbitdl:/downloads
+          restart: unless-stopped" >> $qbitanswer
+    			echo " " >> $qbitanswer
+    			echo "Done."
+  			echo " "
+  		elif [ "$fix" = n ]; then
+  			echo "Not adding qBittorrent to any file."
+  			source torrent-client-installer.sh
+  			return
+  		else
+  			echo "Goodbye!"
+  			exit 1
+  		fi
 elif [ "$yesorno" = e ]; then
 	echo "Goodbye!"
 	exit 1
@@ -122,9 +153,9 @@ fi
 
 #Install Transmission
 
-echo "Would you like install Transmission? (y/n/e)"
+echo "Would you like install Transmission? (y/n/f/e)"
 
-read yesorno
+read -n1 yesorno
 
 if [ "$yesorno" = y ]; then
 	mkdir /home/$USER/raspi-docker/transmission
@@ -152,6 +183,46 @@ if [ "$yesorno" = y ]; then
 	echo "Successfully Added"
 elif [ "$yesorno" = n ]; then
 	echo "Skipping..."
+elif [ "$yesorno" = f ]; then
+        echo " "
+        read -n1 -p "You have selected to change the location/volumes of the container. Would you like to coninue? (y/n) " fix
+    if [ "$fix" = y ]; then
+      echo " "
+      read -p "Enter the location of the docker-compose.yml file: " transmissionanswer
+      read -p "Enter the new location for config: " transmissionconfig
+      read -p "Enter the new location for downloads: " transmissiondl
+      sleep 1
+      echo "transmission:
+        image: linuxserver/transmission
+        container_name: transmission
+        environment:
+          - PUID=1000
+          - PGID=1000
+          - TZ=UTC
+          - TRANSMISSION_WEB_HOME=/combustion-release/ #optional
+    #      - USER=username #optional
+    #      - PASS=password #optional
+        volumes:
+          - $transmissionconfig:/config
+          - $transmissiondl:/downloads
+          - ./downloads/TransmissionWatchFolder:/watch
+        ports:
+          - 9091:9091
+          - 51413:51413
+          - 51413:51413/udp
+        restart: unless-stopped" >> $transmissionanswer
+        echo " " >> $transmissionanswer
+        echo "Done."
+        echo "To add a username and password, edit the docker-config.yml file after adding. Disabled by default."
+      echo " "
+    elif [ "$fix" = n ]; then
+      echo "Not adding Transmission to any file."
+      source torrent-client-installer.sh
+      return
+    else
+      echo "Goodbye!"
+      exit 1
+    fi
 elif [ "$yesorno" = e ]; then
 	echo "Goodbye!"
 	exit 1
@@ -164,9 +235,9 @@ fi
 
 #Install Deluge
 
-echo "Would you like install Deluge? (y/n/e)"
+echo "Would you like install Deluge? (y/n/f/e)"
 
-read yesorno
+read -n1 yesorno
 
 if [ "$yesorno" = y ]; then
 	mkdir /home/$USER/raspi-docker/deluge
@@ -192,6 +263,43 @@ if [ "$yesorno" = y ]; then
 	echo "Successfully Added"
 elif [ "$yesorno" = n ]; then
 	echo "Skipping..."
+elif [ "$yesorno" = f ]; then
+          echo " "
+          read -n1 -p "You have selected to change the location/volumes of the container. Would you like to coninue? (y/n) " fix
+  		if [ "$fix" = y ]; then
+        echo " "
+        read -p "Enter the location of the docker-compose.yml file: " delugeanswer
+  			read -p "Enter the new location for config: " delugeconfig
+        read -p "Enter the new location for downloads: " delugedl
+  			sleep 1
+  			echo "deluge:
+          image: linuxserver/deluge
+          container_name: deluge
+          # network_mode: host
+          environment:
+            - PUID=1000
+            - PGID=1000
+            - TZ=UTC
+            - UMASK=022 #optional
+            - DELUGE_LOGLEVEL=error #optional
+          volumes:
+            - $delugeconfig:/config
+            - $delugedl:/downloads
+          ports:
+            - 8112:8112
+            - 58846:58846
+            - 58946:58946" >> $delugeanswer
+    			echo " " >> $delugeanswer
+    			echo "Done."
+  			echo " "
+  		elif [ "$fix" = n ]; then
+  			echo "Not adding Deluge to any file."
+  			source torrent-client-installer.sh
+  			return
+  		else
+  			echo "Goodbye!"
+  			exit 1
+  		fi
 elif [ "$yesorno" = e ]; then
 	echo "Goodbye!"
 	exit 1
@@ -209,4 +317,4 @@ echo " "
 echo "Installer Complete."
 echo "To access Portainer, go to the IP of this device in a web-browser, port 9000. Ex: 192.168.1.18:9000"
 echo " "
-echo "Goodbye!" 
+echo "Goodbye!"
